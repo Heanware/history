e2c23beb8d26d4f719fc6625f35621bcdb215ee4
heanware <heanware@gmail.com>
2022-11-07 18:41:13 +0500

    add js timeline

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,13 +3,25 @@ const $window = $(window);
 class AnimatedTimeline{
     $wrapper;
     $bar;
+    $points;
 
     constructor($wrapper){
         this.$wrapper = $wrapper;
         this.$bar = $wrapper.find(".js-timeline-bar");
+        this.$points = $wrapper.find(".js-timeline-point");
         let thisTimeline = this;
         this.$wrapper.on("scroll", function(){
-
+            let scroll = $(this).scrollTop();
+            console.log(thisTimeline.$wrapper.height() );
+            thisTimeline.$bar.css("height", thisTimeline.$wrapper.height / scroll + "%");
+            thisTimeline.$points.each(function(){
+                let $point = $(this);
+                if($point.position().top <= scroll){
+                    $point.addClass("point-active");
+                }else{
+                    $point.removeClass("point-active");
+                }
+            })
         });
     }
 }
