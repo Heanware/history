d3200522a04b0262b50912c9391a57c8bef0affa
heanware <heanware@gmail.com>
2022-11-07 18:16:32 +0500

    add js class

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,23 @@
+const $window = $(window);
+
+class AnimatedTimeline{
+    $wrapper;
+    $bar;
+
+    constructor($wrapper){
+        this.$wrapper = $wrapper;
+        this.$bar = $wrapper.find(".js-timeline-bar");
+        let thisTimeline = this;
+        this.$wrapper.on("scroll", function(){
+
+        });
+    }
+}
+
 $(function(){
-    let effect = new Rellax(".rellax-img", {speed: 1});
+    let effect = new Rellax(".rellax-img", {speed: 1}),
+        timelines = $(".js-timeline");
+    timelines.each(function(){
+        new AnimatedTimeline($(this));
+    });
 })
