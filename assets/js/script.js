5ce862e584a3e03434b7f86dae1f142c4e2690d7
heanware <heanware@gmail.com>
2022-11-08 17:57:11 +0500

    fix js timeline

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,32 +3,34 @@ const $window = $(window);
 class AnimatedTimeline{
     $wrapper;
     $bar;
-    $points;
+    $items;
 
     constructor($wrapper){
         this.$wrapper = $wrapper;
         this.$bar = $wrapper.find(".js-timeline-bar");
-        this.$points = $wrapper.find(".js-timeline-point");
+        this.$items = $wrapper.find(".js-timeline-item");
         let thisTimeline = this;
         this.$wrapper.on("scroll", function(){
-            let scroll = $(this).scrollTop();
-            console.log(thisTimeline.$wrapper.height() );
-            thisTimeline.$bar.css("height", thisTimeline.$wrapper.height / scroll + "%");
-            thisTimeline.$points.each(function(){
-                let $point = $(this);
-                if($point.position().top <= scroll){
-                    $point.addClass("point-active");
-                }else{
-                    $point.removeClass("point-active");
-                }
-            })
+            let $scrollingBlock = thisTimeline.$wrapper.find(".js-timeline-scrolling"),
+                scroll = $(this).scrollTop(),
+                actualHeight = Math.ceil(parseInt($scrollingBlock.innerHeight()) - parseInt(thisTimeline.$wrapper.css("height"))),
+                firstItemHeight = thisTimeline.$items.first().height(),
+                progressPercentage = firstItemHeight + Math.ceil(actualHeight / 100) * ((scroll / actualHeight) * 100);
+            thisTimeline.$bar.css("height", progressPercentage);
+            thisTimeline.$items.each(function(){
+                let $item = $(this);
+                if($item.find(".js-timeline-point").position().top < progressPercentage)
+                    $item.addClass("item-active");
+                else
+                    $item.removeClass("item-active");
+            });
         });
+        this.$wrapper.trigger("scroll");
     }
 }
 
 $(function(){
-    let effect = new Rellax(".rellax-img", {speed: 1}),
-        timelines = $(".js-timeline");
+    let effect = new Rellax(".rellax-img", {speed: 2}), timelines = $(".js-timeline");
     timelines.each(function(){
         new AnimatedTimeline($(this));
     });
